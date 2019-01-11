82d2b4cb14dadbe13cbf0150a401163bea0469ff
Douglas Brandão <38235074+douglasbrandao21@users.noreply.github.com>
2019-01-11 13:35:46 -0200

    Add files via upload

--- vesco/js/custom.js
+++ vesco/js/custom.js
@@ -2,7 +2,91 @@
                             SERVICES   
 =============================================================*/
 
-$(function() {
+$(function () {
     //animate on scroll
     new WOW().init();
-})
+});
+
+/*=============================================================
+                            MAGNIFIC  
+=============================================================*/
+
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
+});
+
+$(function () {
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
+
+$(function () {
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
+
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000,
+    });
+});
+
+$(function () {
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+    });
+});
+
+$(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() < 50){
+            $("nav").removeClass("navbar-vesco-top");
+        }
+        else{
+           $("nav").addClass("navbar-vesco-top");
+        }  
+    })
+});
+
+
+
+
+
+
+document.querySelectorAll("a[href^=\"#\"]").forEach((anchor) => {
+  anchor.addEventListener("click", function (ev) {
+    ev.preventDefault();
+
+    const targetElement = document.querySelector(this.getAttribute("href"));
+    targetElement.scrollIntoView({
+        block: "start",
+        alignToTop: true,
+        behavior: "smooth"
+    });
+  });
+});
+
+
+
+
